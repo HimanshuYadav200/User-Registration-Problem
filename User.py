92e36c1991dc8be63bf457a4780cfd0b179ebdca
HimanshuYadav200 <hs8343@srmist.edu.in>
2025-02-11 11:53:04 +0530

    [Himanshu] Add: This python script contains code for validating last name  of user

--- User.py
+++ User.py
@@ -1,12 +1,12 @@
 import re
 import logging
 
-logging.basicConfig(filename="firstname_validation.log", level=logging.DEBUG,
+logging.basicConfig(filename="sample.log", level=logging.DEBUG,
                     format="%(asctime)s - %(levelname)s - %(message)s")
 
 def validate_user_name(first_name):
     
-    logging.debug(f"received input for validation: {first_name}")
+    logging.debug(f"received input for alidation: {first_name}")
     
     pattern=r"^[A-Z].{2,}$"
     
@@ -25,17 +25,52 @@ def validate_user_name(first_name):
         logging.warning(f"Invalid username attempt: {first_name}")
         return False  
     
+
+
+def validate_user_lastname(last_name):
+    
+    logging.debug(f"received input for validation: {last_name}")
+    
+    pattern=r"^[A-Z].{2,}$"
+    
+    if not last_name:
+        logging.critical("Empty username input detected")
+        return False
+    
+    if len(last_name)<3:
+        logging .error(f"username too short: {last_name}")
+        return False
+        
+    if re.match(pattern,last_name):
+        logging.info(f"Valid username: {last_name}")
+        return True
+    else:
+        logging.warning(f"Invalid username attempt: {last_name}")
+        return False  
+    
+    
+
+ 
+    
+    
     
 
 
     
     
 if __name__=="__main__":
+    
     initial_name=input("Enter first name: ")
 if validate_user_name(initial_name):
     print(f"{initial_name} is valid user name")
 else:
     print("invalid user name")  
+    
+sur_name=input("Enter last name: ")
+if validate_user_lastname(sur_name):
+    print(f"{sur_name} is valid user name")
+else:
+    print("invalid user name")   
     
     
         
